7e4d073ce6c6db234336a23e5090f8fd31c6e7d6
marta_vitos <martavitos20@gmail.com>
2020-10-05 22:51:23 +0300

    GameOver

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject endText;
+    public GameObject blackScreen;
+    public GameObject creditsText;
+
+    public IEnumerator PlayGameOver()
+    {
+        endText.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        blackScreen.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        creditsText.SetActive(true);
+
+    }
+}
