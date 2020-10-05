5443caf81d7acd9d30fa60fb9c54326513ddd4c6
marta_vitos <martavitos20@gmail.com>
2020-10-05 20:42:11 +0300

    Spawner Manager.

---
+++ Assets/Managers/SpawnerManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public static SpawnerManager instance;
+
+    [SerializeField]
+    private List<ItemTransform> allItemsOnMapToReset;
+
+    public void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+        InitializeManager();
+    }
+
+    private void InitializeManager()
+    {
+        GameManager.instance.onReset += OnWorldReset;
+    }
+
+    public void OnWorldReset(int itteration)
+    {
+        foreach (ItemTransform item in allItemsOnMapToReset)
+        {
+            Instantiate(item.item, item.whereToPlace.position, item.whereToPlace.localRotation);
+        }
+    }
+
+    [System.Serializable]
+    public class ItemTransform
+    {
+        public GameObject item;
+        public Transform whereToPlace;
+    }
+
+}
